5c5504e83892c73d3961bf85735a4d074df71d35
Yasmin Vargas <91469889+yasmin-vargas@users.noreply.github.com>
2023-11-19 23:56:31 +0100

    Add files via upload

--- src/Screens/OutfitCalendarScreen.js
+++ src/Screens/OutfitCalendarScreen.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import { View } from 'react-native';
 import OutfitCalendar from '../Components/OutfitCalendar';
 function OutfitCalendarScreen({ navigation }) {
+    // example outfitData, creates an object with dates and associated outfits
+    const outfitData = {
+        '2023-11-02': ['Outfit 1', 'Outfit 2'],
+        '2023-11-07': ['Outfit 3'],
+    };
     return (
         <View>
-            <OutfitCalendar/>
+            <OutfitCalendar outfitData={outfitData}/>
         </View>
     );
 }
