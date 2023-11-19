274673b8579fd504b7107a4c5ef086eaddf444ee
Yasmin Vargas <91469889+yasmin-vargas@users.noreply.github.com>
2023-11-19 13:40:05 +0100

    Add files via upload

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,19 +1,20 @@
+import { useEffect, useState } from 'react';
 import { View, Text, Image } from 'react-native';
+import { NativeBaseProvider, Select, Center, FormControl, CheckIcon, WarningOutlineIcon, Button } from "native-base";
+import { Styles } from '../Styles/Stylesheet';
 import Weather from '../Components/Weather';
 import WeatherIcon from '../Components/WeatherIcon';
-import { useEffect, useState } from 'react';
-import { Select, Center, FormControl, CheckIcon, WarningOutlineIcon, NativeBaseProvider, Button } from "native-base";
-import { Styles } from '../Styles/Stylesheet';
 import PredictionDialog from '../Components/PredictionDialog';
-
 export function Example({ selection, label, setSelection }) {
     return (
         <FormControl w="3/4" maxW="300" isRequired isInvalid>
-            <FormControl.Label > <span style={Styles.predictionForm.text}>{label}</span></FormControl.Label>
+            <FormControl.Label >
+                <Text style={Styles.predictionForm.text}>{label}</Text>
+            </FormControl.Label>
             <Select minWidth="200"
-                accessibilityLabel="Choose option"
-                onValueChange={(e) => setSelection(e)}
-                mt="1">
+                    accessibilityLabel="Choose option"
+                    onValueChange={(e) => setSelection(e)}
+                    mt="1">
                 {selection && (
                     selection.map(option => {
                         return <Select.Item label={option} value={option.toString()} />
@@ -21,21 +22,20 @@ export function Example({ selection, label, setSelection }) {
                 )}
             </Select>
             <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" style={Styles.predictionForm.text} />} >
-                <span style={Styles.predictionForm.text}> Please make a selection!</span>
+                <Text style={Styles.predictionForm.text}> Please make a selection!</Text>
             </FormControl.ErrorMessage>
         </FormControl>
     );
 };
 
-
 function HomeScreen({ navigation }) {
     const [mood, setMood] = useState('');
     const [activity, setActivity] = useState('');
-    const [comfortLevel, setComfotLevel] = useState('');
+    const [comfortLevel, setComfortLevel] = useState('');
     const [showModal, setShowModal] = useState(false);
 
     const moodSelection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const activitySelection = ['Date', 'Work', 'Birthday', 'Party', 'Casual'];
+    const activitySelection = ['Work', 'Relax', 'Outside', 'University', 'Friends'];
     const comfortSelection = [1, 2, 3, 4, 5];
     const data = {
         mood: mood,
@@ -44,7 +44,7 @@ function HomeScreen({ navigation }) {
     }
 
     const handleSubmitData = () => {
-                //get prediction here
+        //get prediction here
         /*  axios.post('http://127.0.0.1:8000/getPrediction/', data)
                  .then(response => {
                      console.log(response);
@@ -52,13 +52,12 @@ function HomeScreen({ navigation }) {
                  .catch(e => console.log(e)) */
 
         console.log(data);
-        
+
         //Show data
         setShowModal(true);
     }
 
     return (
-
         <View>
             <NativeBaseProvider>
                 <Center>
@@ -68,18 +67,19 @@ function HomeScreen({ navigation }) {
                     <View style={Styles.predictionForm.container}>
                         <Example selection={moodSelection} label="Choose your mood" setSelection={setMood} />
                         <Example selection={activitySelection} label="Choose activity" setSelection={setActivity} />
-                        <Example selection={comfortSelection} label="Choose desired comfort level" setSelection={setComfotLevel} />
-                        <Button colorScheme="secondary" onPress={() => handleSubmitData()} style={{ margin: '1rem' }}> Submit</Button>
+                        <Example selection={comfortSelection} label="Choose desired comfort level" setSelection={setComfortLevel} />
+                        <Button colorScheme="secondary" onPress={() => handleSubmitData()} style={Styles.predictionForm.button}> Submit</Button>
                     </View>
                     <PredictionDialog prediction={data} showModal={showModal} setShowModal={setShowModal} />
                 </Center>
-
             </NativeBaseProvider>
         </View>
 
     );
 }
 export default HomeScreen;
+
+
 
 
 
