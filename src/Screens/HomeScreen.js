050c1ec6c295f5d2266bb7c615b65d744faab37d
Yasmin Vargas <91469889+yasmin-vargas@users.noreply.github.com>
2023-11-20 20:23:55 +0100

    Add files via upload
    Error message disappears when value is selected. WeatherIcon is commented out since it doesnt work.

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,35 +1,41 @@
-import { useEffect, useState } from 'react';
-import { View, Text, Image } from 'react-native';
+import { useState } from 'react';
+import { View, Text } from 'react-native';
 import { Styles } from '../Styles/Stylesheet';
-import { NativeBaseProvider, Select, FormControl, CheckIcon, WarningOutlineIcon, Button } from "native-base";
+import { NativeBaseProvider, Select, FormControl, WarningOutlineIcon, Button } from "native-base";
 import Weather from '../Components/Weather';
 import WeatherIcon from '../Components/WeatherIcon';
 import PredictionDialog from '../Components/PredictionDialog';
 export function Example({ selection, label, setSelection }) {
+    const [selectedValue, setSelectedValue] = useState('');
+    const [error, setError] = useState(true); // Initially set to true to show the error
+    const handleValueChange = (e) => {
+        const parsedValue = typeof e === 'string' ? e : e.toString();
+        setSelection(parsedValue);
+        setError(parsedValue === ''); // Set error to true if no value is selected
+        setSelection(parsedValue);
+    };
     return (
-        <FormControl w="3/4" maxW="300" isRequired isInvalid>
+        <FormControl w="3/4" maxW="300" isInvalid={error}>
             <FormControl.Label >
                 <Text style={Styles.formStyles.labelText}>{label}</Text>
             </FormControl.Label>
             <Select minWidth="200"
                     accessibilityLabel="Choose option"
-                    onValueChange={(e) => {
-                        const parsedValue = typeof e === 'string' ? e : e.toString();
-                        setSelection(parsedValue);
-                    }}
-                    mt="1">
-                {selection && (
-                    selection.map(option => {
-                        return <Select.Item key={option} label={option.toString()} value={option.toString()} />
-                    })
-                )}
+                    onValueChange={(e) => handleValueChange(e)}
+                    value={selectedValue}
+                    mt="1"
+            >
+                {selection &&
+                    selection.map(option => (
+                        <Select.Item key={option} label={option.toString()} value={option.toString()} />
+                    ))}
             </Select>
             <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" style={Styles.formStyles.text} />} >
                 <Text style={Styles.formStyles.text}> Please make a selection!</Text>
             </FormControl.ErrorMessage>
         </FormControl>
     );
-};
+}
 
 function HomeScreen({ navigation }) {
     const [mood, setMood] = useState(1);
@@ -45,7 +51,6 @@ function HomeScreen({ navigation }) {
         activity: activity,
         weather: comfortLevel
     }
-
     const handleSubmitData = () => {
         //get prediction here
         /*  axios.post('http://127.0.0.1:8000/getPrediction/', data)
@@ -53,9 +58,7 @@ function HomeScreen({ navigation }) {
                      console.log(response);
                  })
                  .catch(e => console.log(e)) */
-
         console.log(data);
-
         //Show data
         setShowModal(true);
     }
@@ -63,15 +66,15 @@ function HomeScreen({ navigation }) {
     return (
         <View style={Styles.weatherStyles}>
             <NativeBaseProvider>
-                    <Text>Welcome to What2Wear Outfit Planner app!</Text>
-                    <Weather />
-                    <WeatherIcon />
-                    <View style={Styles.formStyles.container}>
-                        <Example selection={moodSelection} label="Choose your mood" setSelection={setMood} />
-                        <Example selection={activitySelection} label="Choose activity" setSelection={setActivity} />
-                        <Example selection={comfortSelection} label="Choose desired comfort level" setSelection={setComfortLevel} />
-                        <Button colorScheme="secondary" onPress={() => handleSubmitData()} style={Styles.formStyles.button}> Submit</Button>
-                    </View>
+               <Text>Welcome to What2Wear Outfit Planner app!</Text>
+                {/* <WeatherIcon /> */}
+                <Weather />
+                <View style={Styles.formStyles.container}>
+                   <Example selection={moodSelection} label="Choose your mood" setSelection={setMood} />
+                   <Example selection={activitySelection} label="Choose activity" setSelection={setActivity} />
+                   <Example selection={comfortSelection} label="Choose desired comfort level" setSelection={setComfortLevel} />
+                   <Button colorScheme="secondary" onPress={() => handleSubmitData()} style={Styles.formStyles.button}> Submit</Button>
+                </View>
             </NativeBaseProvider>
         </View>
     );
