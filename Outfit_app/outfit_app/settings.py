035acb42672a83156b117c0116f1178b5fcb692d
Nikki Dilling <ndillinghansen@gmail.com>
2023-11-20 20:19:27 +0100

    fixed comment in the backend

--- Outfit_app/outfit_app/settings.py
+++ Outfit_app/outfit_app/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:19006", # Assuming your frontend is running on localhost:3000
+    "http://localhost:19006", # Assuming your frontend is running on localhost:19006
     "https://www.getpostman.com" 
 
 ]
